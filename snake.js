a0b89bfe5e98a2d031112c8bc7f51d10b5345c75
ahmadalics <ahmadalics@gmail.com>
2023-10-06 22:56:02 -0500

    Adding Test

--- snake.js
+++ snake.js
@@ -1,3 +1,29 @@
+// Set up the touch controls
+let touchStartX = 0;
+let touchStartY = 0;
+document.addEventListener('touchstart', function (e) {
+  touchStartX = e.touches[0].clientX;
+  touchStartY = e.touches[0].clientY;
+});
+document.addEventListener('touchend', function (e) {
+  let touchEndX = e.changedTouches[0].clientX;
+  let touchEndY = e.changedTouches[0].clientY;
+  let deltaX = touchEndX - touchStartX;
+  let deltaY = touchEndY - touchStartY;
+  if (Math.abs(deltaX) > Math.abs(deltaY)) {
+    if (deltaX > 0 && direction !== 'left') {
+      direction = 'right';
+    } else if (deltaX < 0 && direction !== 'right') {
+      direction = 'left';
+    }
+  } else {
+    if (deltaY > 0 && direction !== 'up') {
+      direction = 'down';
+    } else if (deltaY < 0 && direction !== 'down') {
+      direction = 'up';
+    }
+  }
+});
 // Set up the canvas
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
@@ -7,6 +33,7 @@ const height = canvas.height;
 // Set up the game state
 let snake = [{x: 10, y: 10}];
 let food = {x: 0, y: 0};
+let obstacles = [{x: 5, y: 5}, {x: 15, y: 15}]; // add some obstacles
 let direction = 'right';
 let score = 0;
 let gameover = false;
@@ -60,8 +87,24 @@ function gameLoop() {
         }
     }
 
+      // Check for collision with walls, obstacles, or self
+  if (head.x < 0 || head.x >= width / 10 || head.y < 0 || head.y >= height / 10) {
+    gameover = true;
+  }
+  for (let i = 0; i < obstacles.length; i++) {
+    if (head.x === obstacles[i].x && head.y === obstacles[i].y) {
+      gameover = true;
+    }
+  }
+  for (let i = 1; i < snake.length; i++) {
+    if (head.x === snake[i].x && head.y === snake[i].y) {
+      gameover = true;
+    }
+  }
+
+
     // Draw the game
-    ctx.clearRect(0, 0, width, height);
+   ctx.clearRect(0, 0, width, height);
     ctx.fillStyle = 'green';
     for (let i = 0; i < snake.length; i++) {
         ctx.fillRect(snake[i].x * 10, snake[i].y * 10, 10, 10);
@@ -69,6 +112,14 @@ function gameLoop() {
     ctx.fillStyle = 'red';
     ctx.fillRect(food.x * 10, food.y * 10, 10, 10);
     ctx.fillStyle = 'black';
+    ctx.fillText(`Score: ${score}`, 10, 20);
+
+    ctx.fillStyle = 'gray'; // add some color for the obstacles
+    for (let i = 0; i < obstacles.length; i++) {
+      ctx.fillRect(obstacles[i].x * 10, obstacles[i].y * 10, 10, 10);
+    }
+    ctx.fillStyle = 'white';
+    ctx.font = '24px sans-serif';
     ctx.fillText(`Score: ${score}`, 10, 20);
 
     // Schedule the next frame
